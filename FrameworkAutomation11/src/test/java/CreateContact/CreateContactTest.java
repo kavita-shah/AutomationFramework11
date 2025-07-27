8ef8cf6f0301c74311839e741753cae21bb6d742
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-27 21:40:52 +0530

    class added

--- FrameworkAutomation11/src/test/java/CreateContact/CreateContactTest.java
+++ FrameworkAutomation11/src/test/java/CreateContact/CreateContactTest.java
@@ -1,11 +1,14 @@
 package CreateContact;
 
-public class CreateContactTest {
 
-	public static void main(String[] args) {
+public class CreateContactTest {
+	public static void main(String[] args)
+	 {
 		// TODO Auto-generated method stub
 		System.out.println("Execute CreateContact");
 
 	}
 
+   
+	
 }
---
+++ FrameworkAutomation11/src/test/java/CreateContact/CreateDate.java
@@ -0,0 +1,11 @@
+package CreateContact;
+
+public class CreateDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Execute ContactWithDate");
+
+	}
+
+}
